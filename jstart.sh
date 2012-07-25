7c4b648ce24dcdde8650c7440d3db452229b007b
dmn'n <ddotmnn+git@gmail.com>
2012-07-26 00:40:21 +0200

    first commit

---
+++ jstart.sh
@@ -0,0 +1,11 @@
+CHROME_PATH='C:\Users\Damien\AppData\Local\Google\Chrome SxS\Application\chrome.exe' #change with current local path
+
+if [ "$(echo $1 | grep "^[ [:digit:] ]*$")" ] #if numeric launch server with $1 as port number
+then 
+	python -m SimpleHTTPServer $1 &
+	param=http://localhost:$1/
+else 
+	param=`pwd`/"${1:-index.html}" #set param to "current_path/index.html" if $1 is null
+fi
+
+"$CHROME_PATH" $param -allow-file-access-from-files & livereload
